a8a3ffc2a088cc99392970ace7addb961a222ecf
towa0 <126798279+towa0@users.noreply.github.com>
2023-10-29 19:43:16 +0400

    Update express-api.js

--- express-api.js
+++ express-api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const Car = require("/carSchema"); 
+const Car = require("./carSchema"); 
 const cors = require("cors");
 
 const app = express();
