12983412fa77e1c66a0f515e1b1f0720e0f168ab
towa0 <126798279+towa0@users.noreply.github.com>
2023-10-30 10:52:06 +0400

    Update express-api.js

--- express-api.js
+++ express-api.js
@@ -10,13 +10,16 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("MongoDB connection error:", err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => {
+  console.error("MongoDB connection error:", err);
+  throw err;
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hallo");
